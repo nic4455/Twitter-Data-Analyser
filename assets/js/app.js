d9402a260d8c372a3311e580872e25ac5f360cf9
Speedway <harpalassi@gmail.com>
2018-11-15 21:28:34 -0600

    added date and time via momentjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 
-// ajax call to news aggregator API to get an object array of Fox News top headlines.
 var queryURL = 'https://newsapi.org/v2/top-headlines?' +
           'sources=fox-news&' +
           'apiKey=1b5b156f36164922908fb04b4a91da79';
 var articles;
+
+// function for ajax call to news aggregator API to get an object array of Fox News top headlines.
 
 function getHeadlines() {
     $.ajax({
@@ -13,14 +14,15 @@ function getHeadlines() {
             articles = response.articles;
         console.log(articles); 
             for (var i = 0; i < articles.length; i++) {
-            var newList = $("<ul>");
-            var headlines = $("<li>");
-            headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>
-            <p>${moment.utc(articles[i].publishedAt)}</p>`);
-            newList.append(headlines);    
-        $(".headlines").append(newList); 
+                var newList = $("<ul>");
+                var headlines = $("<li>");
+                headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>
+                <p>${moment.utc(articles[i].publishedAt).format('MMMM DD, HH:mm a')}</p>`);
+                newList.append(headlines);    
+                $(".headlines").append(newList); 
         }
     });
 }
+
 
 getHeadlines();
