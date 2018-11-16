a6399bb9a8adc7446c678d82d141ed5a3017935d
Speedway <harpalassi@gmail.com>
2018-11-15 21:15:27 -0600

    added date and time via momentjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,18 +5,22 @@ var queryURL = 'https://newsapi.org/v2/top-headlines?' +
           'apiKey=1b5b156f36164922908fb04b4a91da79';
 var articles;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response) {
-        articles = response.articles;
-    console.log(articles); 
-        for (var i = 0; i < articles.length; i++) {
-        var newList = $("<ul>");
-        var headlines = $("<li>");
-        headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>`);
-        newList.append(headlines);    
-    $(".headlines").append(newList); 
-    }
-});
+function getHeadlines() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+            articles = response.articles;
+        console.log(articles); 
+            for (var i = 0; i < articles.length; i++) {
+            var newList = $("<ul>");
+            var headlines = $("<li>");
+            headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>
+            <p>${moment.utc(articles[i].publishedAt)}</p>`);
+            newList.append(headlines);    
+        $(".headlines").append(newList); 
+        }
+    });
+}
 
+getHeadlines();
