3a9314f4648bde071471f5b51259f9ae24d7c24f
Speedway <harpalassi@gmail.com>
2018-11-15 20:34:36 -0600

    ajax call to receive headlines in div

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+
+// ajax call to news aggregator API to get an object array of Fox News top headlines.
+var queryURL = 'https://newsapi.org/v2/top-headlines?' +
+          'sources=fox-news&' +
+          'apiKey=1b5b156f36164922908fb04b4a91da79';
+var articles;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+        articles = response.articles;
+    console.log(articles); 
+        for (var i = 0; i < articles.length; i++) {
+        var newList = $("<ul>");
+        var headlines = $("<li>");
+        headlines.append(articles[i].title);
+        newList.append(headlines);    
+    $(".headlines").append(newList); 
+    }
+});
+
