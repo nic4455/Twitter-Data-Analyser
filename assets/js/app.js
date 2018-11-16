4eb6e932df351913af6c6cab2150ffd97db45e11
Speedway <harpalassi@gmail.com>
2018-11-16 00:31:15 -0600

    updated time to CST

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ var queryURL = 'https://newsapi.org/v2/top-headlines?' +
           'apiKey=1b5b156f36164922908fb04b4a91da79';
 var articles;
 
-// function for ajax call to news aggregator API to get an object array of Fox News top headlines.
+// function for ajax call to news  API to get an array of Fox News top headlines.
 
 function getHeadlines() {
     $.ajax({
@@ -12,17 +12,17 @@ function getHeadlines() {
         method: "GET"
         }).then(function(response) {
             articles = response.articles;
-        console.log(articles); 
+            console.log(articles); 
             for (var i = 0; i < articles.length; i++) {
-                var newList = $("<ul>");
-                var headlines = $("<li>");
-                headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>
-                <p>${moment.utc(articles[i].publishedAt).format('MMMM DD, HH:mm a')}</p>`);
-                newList.append(headlines);    
-                $(".headlines").append(newList); 
+                    var newList = $("<ul>");
+                    var headlines = $("<li>");
+                    headlines.append(`<a href="${articles[i].url}" target="_blank"> ${articles[i].title} </a>
+                    <p>Posted: ${moment.utc(articles[i].publishedAt).subtract(6, "hours").format('MMMM DD, h:mm a')}</p>`);
+                    newList.append(headlines);    
+                    $(".headlines").append(newList); 
         }
     });
-}
+} 
 
 
 getHeadlines();
