624f66e335a5da337186a39fbe587d1eebb53b12
Speedway <harpalassi@gmail.com>
2018-11-21 12:11:35 -0600

    added news api code and momentjs cdn

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,45 @@ var config = {
     messagingSenderId: "316462943623"
 };
 firebase.initializeApp(config);
+
+
+
+// news apikey and query url set up
+var queryURL = 'https://newsapi.org/v2/top-headlines?' +
+          'sources=fox-news&' +
+          'apiKey=1b5b156f36164922908fb04b4a91da79';
+var articles;
+
+// function for ajax call to get top 10 headlines and put them in a list and append to div.
+// requires jquery and momentjs to run
+
+function getHeadlines() {
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        success: function(response) {
+            articles = response.articles;
+            console.log(articles); 
+            var newList = $("<ul>");
+            for (var i = 0; i < articles.length; i++) {
+                    
+                    var headlines = $("<li>");
+                    headlines.append(`<a href="${articles[i].url}" target="_blank">${articles[i].title} </a>
+                    <p>Posted: ${moment.utc(articles[i].publishedAt).subtract(6, "hours").format('LLLL')}</p>`);
+                    newList.append(headlines);    
+
+                //this is where we would input the div to put the headlines ".headlines"
+                    $(".headlines").append(newList); 
+            } 
+        },
+
+        //error handling message
+        error: function(status, code, message) {
+            $(".headlines").append(`There was an ${code} - ${message}`);
+        },
+    
+    });
+} 
+
+//function to call for the headlines to appear
+getHeadlines();
