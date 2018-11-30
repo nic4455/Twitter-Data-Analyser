7bb864596178ec4f2db1253dff86cb8a7107f65f
Horace JM <nicmay@gmail.com>
2018-11-30 17:41:18 -0600

    added changes to word cloud for responsiveness

--- assets/js/jqcloud.js
+++ assets/js/jqcloud.js
@@ -47,10 +47,9 @@ $(document).ready(function () {
         console.log('normalizedddd ======>', normalizedCloudWords)
 
         $("#cloudContent").jQCloud(bigArray, {
-            width: 1200,
             shape: "rectangular",
             center: {x:0.5, y:0.5},
             autoResize: true
-        });
+        }); 
     });
 })
